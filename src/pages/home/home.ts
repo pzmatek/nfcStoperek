520ef1646de7c49654e0981ec93cc9d04900ccee
unknown <mw@symbio.com.pl>
2018-10-30 18:03:28 +0100

    Pierwszy commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NFC, Ndef } from '@ionic-native/nfc';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,57 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  granted: boolean;
+  denied: boolean;
+  scanned: boolean;
+  tagId: string;
 
+  constructor(public navCtrl: NavController, private nfc: NFC) {
+    this.resetScanData();
+    this.ionViewDidEnter();
+  }
+
+  resetScanData() {
+    this.granted = false;
+    this.scanned = false;
+    this.tagId = "";
+  }
+
+  ionViewDidEnter() {
+    this.nfc.enabled().then((resolve) => {
+      this.addListenNFC();
+    }).catch((reject) => {
+      alert("NFC is not supported by your Device");
+    });
+  }
+
+  addListenNFC() {
+
+    this.nfc.addTagDiscoveredListener(nfcEvent => this.sesReadNFC(nfcEvent.tag)).subscribe(data => {
+      if (data && data.tag && data.tag.id) {
+        let tagId = this.nfc.bytesToHexString(data.tag.id);
+        if (tagId) {
+          this.tagId = tagId;
+          this.scanned = true;
+
+          // only testing data consider to ask web api for access
+          this.granted = [
+            "7d3c6179"
+          ].indexOf(tagId) != -1;
+
+        } else {
+          alert('NFC_NOT_DETECTED');
+        }
+      }
+    });
+  }
+
+  sesReadNFC(data): void {
+
+  }
+
+  failNFC(err) {
+    alert("Error while reading: Please Retry");
   }
 
 }
